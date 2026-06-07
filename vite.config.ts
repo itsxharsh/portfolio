0c7449875db8d6d6498e004eb007aeca3be4920b
Harshdeep Singh <harsh.code97@gmail.com>
2026-06-07 11:45:53 +0530

    feat: add environment configuration, GitHub Actions deployment, and update HTML metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('Loading');
+  console.log(env.VITE_APP_BASE_URL);
+
+  return {
+    plugins: [tailwindcss()],
+    base: env.VITE_APP_BASE_URL,
+  };
 });
